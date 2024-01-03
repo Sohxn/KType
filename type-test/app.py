bb88f77134a8f6672c2942403bfbeed325eae3ae
Sohxn <sohan2004cc@gmail.com>
2024-01-03 15:45:24 +0530

    Added retractable sidebar
    replaced the navbar dropdown with a retractable sidebar that can be closed with the escape key

--- type-test/app.py
+++ type-test/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from essential_generators import DocumentGenerator
+import re
 # pip install essential-generators (comand to install)
 
-sentense  = DocumentGenerator()
+sentence  = DocumentGenerator()
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/data')
 def get_data():
-    raw =  sentense.paragraph()
+    raw =  sentence.paragraph()
+    raw = re.sub(r"[!\"#\$%&\'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`{\|}~]" , "" , raw) #removing punctuations
     data = raw[0:200].split()
     return jsonify(data)
 
--- type-test/src/components/Nav.jsx
+++ type-test/src/components/Nav.jsx
@@ -3,45 +3,83 @@ import '../App.css'
 import prof from '../assets/icons/prf.svg'
 import set from '../assets/icons/set.svg'
 import tr from '../assets/icons/TRicon.png'
-import { useState } from 'react'
+import cbp from '../assets/icons/prf.png'
+import { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 
 
 const Nav = () => {
-  const [ddOpen , setddOpen] =  useState(false)  //initial value is set to false
 
-  const prfopen = () =>
+  const [sbopen, setsbopen] = useState(false)
+
+  const sbtoggle = () =>
   {
-    setddOpen(!ddOpen)
+    setsbopen(!sbopen)
   }
 
+  //escape to close sidebar
+  useEffect(() => {
+    const HandleKeyDown = (event) => {
+      if(sbopen && event.key === 'Escape'){
+        sbtoggle() 
+      }
+      if(!sbopen && event.key === 'Shift'){
+        sbtoggle()
+      }
+    }
 
+  document.addEventListener('keydown' , HandleKeyDown)
+
+  return () => {
+    document.removeEventListener('keydown' , HandleKeyDown)
+    }
+  },
+  [sbopen, sbtoggle])
   
   return (
-    <div className='fixed flex h-[12vh] w-screen justify-center'>
-      <div className='mt-6 rounded-lg w-[70vw] flex container'>
-        <img className='h-14 w-14 self-center' src={tr}/>
-        <h className='font-roboto text-[5vh] text-white ml-[1vw] hover:cursor-pointer self-center'>TypeRabbit. </h>
-
-        
-        <div className='flex  space-x-6 ml-auto mr-6'>
-          <div className='w-12 h-12 self-center'><img src={set}/></div>
-          
-          <div id='profile' className='w-12 h-12 self-center relative' >
-            <button id='dropdownHoverButton' onClick={prfopen}><img className='h-fill w-fill rounded-full' src={prof}/></button>
-            <div className={`prfdd absolute mt-[3vh] bg-opacity-20 border-2 rounded-xl w-fit h-fit max-w-[10vw] m-2 p-[2vh] backdrop-blur-sm ${ddOpen ? '' : 'opacity-0 translate-x-[50vh] hidden'}transition-transform duration-500 ease-in-out transform'}`}>
-              <Link to="/dashboard" className="block px-4 py-2 text-white font-roboto hover:rounded -[7px] hover:bg-white hover:text-black transition-all hover:ease-in-out">Dashboard</Link>
-
-              <Link to="/login" className="block px-4 py-2 text-white font-roboto hover:rounded-[7px]  hover:bg-white hover:text-black transition-all hover:ease-in-out">Log in</Link>
-              <Link to="/" className="block px-4 py-2 text-white font-roboto hover:rounded-[7px]  hover:bg-white hover:text-black transition-all hover:ease-in-out">Type</Link>
-          
-            </div>
-          </div>
+    <div  className='fixed flex h-[12vh] w-screen justify-center'>
+        <div className='mt-6 rounded-lg w-[90vw] flex'>
+          <h className='font-roboto text-[4vh] text-white hover:cursor-pointer self-center'>TypeRabbit. </h>
         </div>
 
-      </div>
+        <div id='buttontoopensidebar' className='text-white font-roboto fixed flex cursor-pointer right-10 top-10'>
+          {sbopen ? (
+           <></>
+          ) : (
+            <>  
+            <button className="rounded-full h-12 w-12 border-4 border-blue-300">
+              <img src={cbp} className='rounded-full' onClick={sbtoggle}/>
+            </button>
+            </>
+            )}
+
+          <div className={`top-0 right-0 w-[15vw] bg-gray-900 p-10 pl-20 text-white fixed h-full z-40 rounded-2xl shadow-[0px_0px_50px_4px_#2d3748] ease-in-out duration-500 ${
+                sbopen ? "translate-x-0" : "translate-x-full"
+          }`}>
+          {/*parent div of sidebar elements*/}
+          <div className='font-roboto'>
+              <div className='text-4xl mb-[5vh]'><span>NAME</span></div>
+              <ul className='mb-[75vh]'>
+                <li>
+                  <Link to="/dashboard">DASH</Link>
+                </li>
+                <li>
+                  <Link to="/login">LOGIN</Link>
+                </li>
+                <li>
+                  <Link to="/">TYPE</Link>
+                </li>
+              </ul>
+              <div>
+                  PRESS <span className='border-2 rounded-md p-1'>ESC</span> TO CLOSE
+              </div>
+          </div>
+          </div>
+        </div>
+       
     </div>
   )
 }
 
 export default Nav
+/*shadow-[0px_0px_50px_4px_#2d3748]*/
