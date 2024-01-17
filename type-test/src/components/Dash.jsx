c907c6fc376b375912d8787796f614bc1ea046cc
Sohxn <sohan2004cc@gmail.com>
2024-01-18 01:11:43 +0530

    DASH ALMOST READY MAST DIKH RAHA

--- type-test/src/components/Dash.jsx
+++ type-test/src/components/Dash.jsx
@@ -5,7 +5,30 @@ import { useState, useEffect } from 'react'
 const Dash = () => {
 
   const [rankOpen, setrankOpen] = useState(false)
-  // const [currentTheme, setcurrentTheme] = useState('default')
+
+  const [currentSpeed, setCurrentSpeed] = useState(0);
+  const targetSpeed = 120 //say
+  const animationDuration = 450
+
+  useEffect(() => {
+    let startTime;
+
+    const animate = (timestamp) => {
+      if (!startTime) startTime = timestamp;
+
+      const progress = Math.min(1, (timestamp - startTime) / animationDuration);
+
+      setCurrentSpeed(Math.floor(progress * targetSpeed));
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    requestAnimationFrame(animate);
+
+    return () => cancelAnimationFrame(animate);
+  }, [targetSpeed, animationDuration]);
   
   const openRanks = () =>
   {
@@ -19,19 +42,22 @@ const Dash = () => {
       <div className='w-[60vw] h-[75vh] m-[10vw] mt-[15vh] rounded-3xl'>
         {/*Masonry layout*/}
         <div className="grid grid-cols-3 gap-10 p-4"  style={{ gridTemplateRows: 'auto auto' }}>
-          <div className="grid-item border-2 h-[35vh] rounded-[35px] text-white">
-          
+          <div className="grid-item flex items-center border-2 h-[35vh] rounded-[35px] text-white bg-purple-400 border-purple-400 justify-center">
+            <span className='text-[10vh] font-roboto'>
+              {currentSpeed}<span className='text-[5vh]'> wpm</span>
+            </span>
           </div>
           <div className="grid-item h-[35vh] rounded-[35px] gap-8 text-white" style={{ display: 'grid', gridTemplateRows: '1fr 1fr' }}>
-            <div className='row-item border-2 rounded-[35px]'></div>
-            <div className='row-item border-2 rounded-[35px]'></div>
+            <div className='row-item flex border-2 rounded-[35px] border-[#e9d5ff] bg-[#e9d5ff] justify-center items-center'>
+              <span className='font-roboto text-[3vh] text-black'>Accuracy</span></div>
+            <div className='row-item border-2 rounded-[35px] border-[#B3B7EE] bg-[#B3B7EE]'></div>
           </div>
 
-          <div className="grid-item border-2 row-span-2 rounded-[35px] text-white">
+          <div className="grid-item border-2 row-span-2 rounded-[35px] text-white border-white bg-white">
             
           </div>
-          <div className="grid-item border-2 h-[30vh] col-span-2 rounded-[35px] text-white">
-            
+          <div className="grid-item flex justify-center items-center border-4 h-[30vh] col-span-2 rounded-[35px] text-white border-[#f5d0fe]">
+             <span className='font-roboto text-[5vh] m-4'>streak will be here</span>
           </div>
         </div>
       </div>
