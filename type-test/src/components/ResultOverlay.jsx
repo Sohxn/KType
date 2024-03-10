a8c48bfc4dfbd84f0cc5b740ffb4d9ce69fde168
Sohxn <sohan2004cc@gmail.com>
2024-03-10 10:29:08 +0530

    Update ResultOverlay.jsx

--- type-test/src/components/ResultOverlay.jsx
+++ type-test/src/components/ResultOverlay.jsx
@@ -77,8 +77,8 @@ const ResultOverlay = (children) => {
     <div className='fixed backdrop-blur-3xl font-roboto bg-transparent top-0 left-0 w-full h-full z-50 in-ease-out duration-300 bg-black ease-in-out'>
         <span className='flex text-white p-5'>press &nbsp; <span className='border-2 rounded-lg text-center'>&nbsp; ctrl + R &nbsp;</span> &nbsp;to type again</span>
         <div className='grid grid-cols-3 text-center mt-20 text-2xl text-white'>
-            <div className='col-span-1'>speed: {}</div>  
-            <div className='col-span-2'>accuracy: {}</div>
+            <div className='col-span-1'>speed:</div>  
+            <div className='col-span-2'>accuracy:</div>
         </div>
     </div>
   )
